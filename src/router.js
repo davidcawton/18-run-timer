116cb2d4275dfffe59281fd9fe14faa399b9099b
David Cawton II <Dvaidc@Davids-MacBook-Pro.local>
2015-12-01 16:08:47 -0600

    Added routes

--- src/router.js
+++ src/router.js
@@ -1,5 +1,22 @@
+import RunlogList from '/..collections/runlog-list';
+import RunIndex from '/..views/posts';
+
 var Router = Backbone.Router.extend({
+  routes: {
+    '': 'allRuns',
+    new: 'newRuns',
+    ':id': 'runsDetail',
+    ':id/edit': 'editRuns',
+  },
+  posts: null,
 
+  initialize() {
+    this.posts = new RunlogList();
+    this.posts.fetch();
+  },
+
+  allRuns() {
+    var runsIndex = new RunIndex({collection: this.posts});
+  },
 });
-
 export default Router;
